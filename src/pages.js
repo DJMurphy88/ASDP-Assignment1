e45a353ff740aabf18ffd712d1e15182d7a52283
Denis Murphy <djmurphy2001@gmail.com>
2022-01-15 14:00:46 -0330

    first commit

---
+++ src/pages.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export function Home() {
+    return (
+        <div>
+            <h1>Home</h1>
+        </div>
+    )
+}
+
+export function Reviews() {
+    return (
+    <div>
+        <h1>Reviews</h1>
+    </div>
+    )
+}
+
+export function Submit() {
+    return (
+    <div>
+        <h1>submit</h1>
+    </div>
+    )
+}
